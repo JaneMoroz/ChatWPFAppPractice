7d19e430cc0bb41df966ae2ecdf33c3a73f6128e
Jane Moroz <Moroz.jane@yahoo.com>
2021-05-10 17:41:47 +0300

    Create Page Animation
    Created page slide from the left animation.

---
+++ ChatApp/Pages/BasePage.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace ChatApp
+{
+    /// <summary>
+    /// Base page for all pages to gain base fynctionality
+    /// </summary>
+    public class BasePage : Page
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The animation the play when the page is first loaded
+        /// </summary>
+        public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
+
+        /// <summary>
+        /// The animation the play when the page is unloaded
+        /// </summary>
+        public PageAnimation PageUnloadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToleft;
+
+        /// <summary>
+        /// The time any slide animation takes to complete
+        /// </summary>
+        public float SlideSeconds { get; set; } = 0.8f;
+
+        #endregion
+
+        #region Constructor
+
+        public BasePage()
+        {
+            // If we are animating in, hide to begin with
+            if (this.PageLoadAnimation != PageAnimation.None)
+            {
+                this.Visibility = Visibility.Collapsed;
+            }
+
+            // Listen out for the page loading
+            this.Loaded += BasePage_Loaded;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Once the page is loaded, perform any required animation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        #region Animation Load / Unload
+
+        private async void BasePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Animate the page in
+            await AnimateIn();
+        }
+
+        /// <summary>
+        /// Animates in this page
+        /// </summary>
+        /// <returns></returns>
+        public async Task AnimateIn()
+        {
+            // Make sure we have smth to do
+            if (this.PageLoadAnimation == PageAnimation.None)
+                return;
+
+            switch (this.PageLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeInFromRight:
+
+                    var sb = new Storyboard();
+                    var slideAnimation = new ThicknessAnimation
+                    {
+                        Duration = new Duration(TimeSpan.FromSeconds(this.SlideSeconds)),
+                        From = new Thickness(this.WindowWidth, 0, -this.WindowWidth, 0),
+                        To = new Thickness(0),
+                        DecelerationRatio = 0.9f
+                    };
+
+                    Storyboard.SetTargetProperty(slideAnimation, new PropertyPath("Margin"));
+                    sb.Children.Add(slideAnimation);
+
+                    sb.Begin(this);
+
+                    this.Visibility = Visibility.Visible;
+
+                    await Task.Delay((int)this.SlideSeconds * 1000);
+
+                    break;
+            }
+        }
+
+        #endregion
+    }
+}
