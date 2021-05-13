a12558334c2a7a6a76f39a78efea4f7e9152eb2a
Jane Moroz <Moroz.jane@yahoo.com>
2021-05-13 18:48:34 +0300

    Fix design time issue

--- ChatApp/AttachedPropertirs/BaseAttachedProperty.cs
+++ ChatApp/AttachedPropertirs/BaseAttachedProperty.cs
@@ -14,7 +14,7 @@ namespace ChatApp
     /// <typeparam name="Parent">The parent class to be the attached property</typeparam>
     /// <typeparam name="Property">The type of this attached property</typeparam>
     public abstract class BaseAttachedProperty<Parent, Property>
-        where Parent : BaseAttachedProperty<Parent, Property>, new()
+        where Parent : new()
     {
         #region Public Events
 
@@ -62,10 +62,10 @@ namespace ChatApp
         private static object OnValuePropertyUpdated(DependencyObject d, object value)
         {
             // Call the parent function
-            Instance.OnValueUpdated(d, value);
+            (Instance as BaseAttachedProperty<Parent, Property>)?.OnValueUpdated(d, value);
 
             // Call event listeners
-            Instance.ValueUpdated(d, value);
+            (Instance as BaseAttachedProperty<Parent, Property>)?.ValueUpdated(d, value);
 
             // Return the value
             return value;
@@ -79,10 +79,10 @@ namespace ChatApp
         private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             // Call the parent function
-            Instance.OnValueChanged(d, e);
+            (Instance as BaseAttachedProperty<Parent, Property>)?.OnValueChanged(d, e);
 
             // Call event listeners
-            Instance.ValueChanged(d, e);
+            (Instance as BaseAttachedProperty<Parent, Property>)?.ValueChanged(d, e);
         }
 
         /// <summary>
