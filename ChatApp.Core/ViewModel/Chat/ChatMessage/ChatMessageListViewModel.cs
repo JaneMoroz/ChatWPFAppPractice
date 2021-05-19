517262a9f18721c3464589e234431127b849ede2
Jane Moroz <Moroz.jane@yahoo.com>
2021-05-19 16:36:42 +0300

    Design Of Base Popup Menu System

--- ChatApp.Core/ViewModel/Chat/ChatMessage/ChatMessageListViewModel.cs
+++ ChatApp.Core/ViewModel/Chat/ChatMessage/ChatMessageListViewModel.cs
@@ -3,18 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ChatApp.Core
 {
-    /// <summary>
-    ///  A view model for a chat message thread list
     /// </summary>
     public class ChatMessageListViewModel : BaseViewModel
     {
+        #region Public Properties
+
         /// <summary>
-        ///  The chat thread items for the list
+        /// The chat thread items for the list
         /// </summary>
         public List<ChatMessageListItemViewModel> Items { get; set; }
 
+        /// <summary>
+        /// True to show the attachment menu, false to hide it
+        /// </summary>
+        public bool AttachmentMenuVisible { get; set; }
+
+        /// <summary>
+        /// The view model for the attachment menu
+        /// </summary>
+        public ChatAttachmentPopupMenuViewModel AttachmentMenu { get; set; }
+
+        #endregion
+
+        #region Public Commands
+
+        /// <summary>
+        /// The command for when the attachment button is clicked
+        /// </summary>
+        public ICommand AttachmentButtonCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ChatMessageListViewModel()
+        {
+            // Create commands
+            AttachmentButtonCommand = new RelayCommand(AttachmentButton);
+
+            // Make a default menu
+            AttachmentMenu = new ChatAttachmentPopupMenuViewModel();
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// When the attachment button is clicked show/hide the attachment popup
+        /// </summary>
+        public void AttachmentButton()
+        {
+            // Toggle menu visibility
+            AttachmentMenuVisible ^= true;
+        }
+
+        #endregion
     }
 }
