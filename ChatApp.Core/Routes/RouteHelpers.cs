b7e079ea9a3f242d23e9caa7aabfd5aabae4d39a
Jane Moroz <Moroz.jane@yahoo.com>
2021-06-10 18:26:14 +0300

    Change User Password

---
+++ ChatApp.Core/Routes/RouteHelpers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Dna.FrameworkDI;
+
+namespace ChatApp.Core
+{
+    /// <summary>
+    /// Helper methods for getting and working with web routes
+    /// </summary>
+    public static class RouteHelpers
+    {
+        /// <summary>
+        /// Converts a relative URL into an absolute URL
+        /// </summary>
+        /// <param name="relativeUrl">The relative URL</param>
+        /// <returns>Returns the absolute URL including the Host URL</returns>
+        public static string GetAbsoluteRoute(string relativeUrl)
+        {
+            // Get the host
+            var host = Configuration["ChatAppServer:HostUrl"];
+
+            // If they are not passing any URL...
+            if (string.IsNullOrEmpty(relativeUrl))
+                // Return the host
+                return host;
+
+            // If the relative URL does not start with /...
+            if (!relativeUrl.StartsWith("/"))
+                // Add the /
+                relativeUrl = $"/{relativeUrl}";
+
+            // Return combined URL
+            return host + relativeUrl;
+        }
+    }
+}
--- ChatApp/ViewModel/Application/SettingsViewModel.cs
+++ ChatApp/ViewModel/Application/SettingsViewModel.cs
@@ -263,7 +263,7 @@ namespace ChatApp
             await UpdateValuesFromLocalStoreAsync();
 
             // Get the user token
-            var token = (await ClientDataStore.GetLoginCredentialsAsync()).Token;
+            var token = (await ClientDataStore.GetLoginCredentialsAsync())?.Token;
 
             // If we don't have a token (so we are not logged in...)
             if (string.IsNullOrEmpty(token))
@@ -272,7 +272,9 @@ namespace ChatApp
 
             // Load user profile details form server
             var result = await WebRequests.PostAsync<ApiResponse<UserProfileDetailsApiModel>>(
-                "http://localhost:5000/api/user/profile",
+                // Set URL
+                RouteHelpers.GetAbsoluteRoute(ApiRoutes.GetUserProfile),
+                // Pass in user Token
                 bearerToken: token);
 
             // If it was successful...
@@ -393,11 +395,61 @@ namespace ChatApp
         /// <returns>Returns true if successful, false otherwise</returns>
         public async Task<bool> SavePasswordAsync()
         {
-            // TODO: Update with server
-            await Task.Delay(3000);
+            // Lock this command to ignore any other requests while processing
+            return await RunCommandAsync(() => PasswordIsChanging, async () =>
+            {
+                // Log it
+                Logger.LogDebugSource($"Changing password...");
 
-            // Return fail
-            return false;
+                // Get the current known credentials
+                var credentials = await ClientDataStore.GetLoginCredentialsAsync();
+
+                // Make sure the user has entered the same password
+                if (Password.NewPassword.Unsecure() != Password.ConfirmPassword.Unsecure())
+                {
+                    // Display error
+                    await UI.ShowMessage(new MessageBoxDialogViewModel
+                    {
+                        // TODO: Localize
+                        Title = "Password Mismatch",
+                        Message = "New password and confirm password must match"
+                    });
+
+                    // Return fail
+                    return false;
+                }
+
+                // Update the server with the new password
+                var result = await WebRequests.PostAsync<ApiResponse>(
+                    // Set URL
+                    RouteHelpers.GetAbsoluteRoute(ApiRoutes.UpdateUserPassword),
+                    // Create API model
+                    new UpdateUserPasswordApiModel
+                    {
+                        CurrentPassword = Password.CurrentPassword.Unsecure(),
+                        NewPassword = Password.NewPassword.Unsecure()
+                    },
+                    // Pass in user Token
+                    bearerToken: credentials.Token);
+
+                // If the response has an error...
+                if (await result.DisplayErrorIfFailedAsync($"Change Password"))
+                {
+                    // Log it
+                    Logger.LogDebugSource($"Failed to change password. {result.ErrorMessage}");
+
+                    // Return false
+                    return false;
+                }
+
+                // Otherwise, we succeeded...
+
+                // Log it
+                Logger.LogDebugSource($"Successfully changed password");
+
+                // Return successful
+                return true;
+            });
         }
 
         #endregion
@@ -472,11 +524,11 @@ namespace ChatApp
 
             // Update the server with the details
             var result = await WebRequests.PostAsync<ApiResponse>(
-                // TODO: Move URLs into better place
-                "http://localhost:5000/api/user/profile/update",
+                // Set URL
+                RouteHelpers.GetAbsoluteRoute(ApiRoutes.UpdateUserProfile),
                 // Pass the Api model
                 updateApiModel,
-                // Create the user details to send
+                // Pass in user Token
                 bearerToken: credentials.Token);
 
             // If the response has an error...
