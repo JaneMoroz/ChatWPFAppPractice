f5f1d58d5eecc1f944a255544e569a22db8969b5
Jane Moroz <Moroz.jane@yahoo.com>
2021-06-11 14:50:29 +0300

    Clean Up Debug Statements

--- ChatApp/ViewModel/Application/SettingsViewModel.cs
+++ ChatApp/ViewModel/Application/SettingsViewModel.cs
@@ -311,15 +311,11 @@ namespace ChatApp
                     // Re-add our known token
                     dataModel.Token = token;
 
-                    Debug.WriteLine($"running");
-
                     // Save the new information in the data store
                     await scopedClientDataStore.SaveLoginCredentialsAsync(dataModel);
 
                     // Update values from local cache
                     await UpdateValuesFromLocalStoreAsync(scopedClientDataStore);
-
-                    Debug.WriteLine($"done");
                 }
             });
 
