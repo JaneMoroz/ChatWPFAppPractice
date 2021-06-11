62ba852be4847411e77f9c513aa7b78587585a10
Jane Moroz <Moroz.jane@yahoo.com>
2021-06-11 14:49:22 +0300

    Fix Bugs
    Fixed double copy of client data store due to transcient scope and fixed lock in RunCommandAsync.

--- ChatApp/ViewModel/Application/SettingsViewModel.cs
+++ ChatApp/ViewModel/Application/SettingsViewModel.cs
@@ -279,12 +279,14 @@ namespace ChatApp
             // Lock this command to ignore any other requests while processing
             await RunCommandAsync(() => SettingsLoading, async () =>
             {
+                // Store single transcient instance of client data store
+                var scopedClientDataStore = ClientDataStore;
 
                 // Update values from local cache
-                await UpdateValuesFromLocalStoreAsync();
+                await UpdateValuesFromLocalStoreAsync(scopedClientDataStore);
 
                 // Get the user token
-                var token = (await ClientDataStore.GetLoginCredentialsAsync())?.Token;
+                var token = (await scopedClientDataStore.GetLoginCredentialsAsync())?.Token;
 
                 // If we don't have a token (so we are not logged in...)
                 if (string.IsNullOrEmpty(token))
@@ -312,10 +314,10 @@ namespace ChatApp
                     Debug.WriteLine($"running");
 
                     // Save the new information in the data store
-                    await ClientDataStore.SaveLoginCredentialsAsync(dataModel);
+                    await scopedClientDataStore.SaveLoginCredentialsAsync(dataModel);
 
                     // Update values from local cache
-                    await UpdateValuesFromLocalStoreAsync();
+                    await UpdateValuesFromLocalStoreAsync(scopedClientDataStore);
 
                     Debug.WriteLine($"done");
                 }
@@ -488,10 +490,10 @@ namespace ChatApp
         /// to this view model
         /// </summary>
         /// <returns></returns>
-        private async Task UpdateValuesFromLocalStoreAsync()
+        private async Task UpdateValuesFromLocalStoreAsync(IClientDataStore clientDataStore)
         {
             // Get the stored credentials
-            var storedCredentials = await ClientDataStore.GetLoginCredentialsAsync();
+            var storedCredentials = await clientDataStore.GetLoginCredentialsAsync();
 
             // Set first name
             FirstName.OriginalText = storedCredentials?.FirstName;
